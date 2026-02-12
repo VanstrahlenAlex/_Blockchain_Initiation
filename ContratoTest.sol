e787289ad42185e095406e6711efa058ce53e49d
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2026-02-12 09:42:20 -0500

    first commit

---
+++ ContratoTest.sol
@@ -0,0 +1,90 @@
+// Licencia 
+// SPDX-License-Identifier: LGPL-3.0-only
+
+//Version 
+pragma solidity ^0.8.33;
+
+//Contrato
+contract ContratoTest {
+     
+//Numeros 
+uint num1 = 1; // [0 ... 2^256 - 1]
+int num2 = -1; // [-2^255 ... 2^255 - 1]
+uint8 num3 = 255; // [0 ... 2^8 - 1]
+uint16 num4 = 65535; // [0 ... 2^16 - 1]
+uint256 num5 = 115792089237316195423570985008687907853269984665640564039457584007913129639935; // [0 ... 2^256 - 1]
+
+int8 num6 = -128; // [-2^7 ... 2^7 - 1]
+int16 num7 = -32768; // [-2^15 ... 2^15 - 1]
+// int256 num8 = -115792089237316195423570985008687907853269984665640564039457584007913129639936; // [-2^255 ... 2^255 - 1]
+
+//Booleanos
+bool bool1 = true;
+bool bool2 = false;
+
+//String
+string string1 = "Hola";
+string string2 = "Mundo";
+
+//Address
+address address1 = 0x0000000000000000000000000000000000000000;
+
+//Array
+uint[] array1 = new uint[](5);
+uint256[5] array2 = [1, 2, 3, 4, 5];
+
+//Mapping
+mapping(address => uint) mapping1;
+mapping(address => uint256) mapping2;
+
+
+
+//Struct
+struct Persona {
+    string nombre;
+    uint edad;
+    address direccion;
+}
+
+Persona persona1 = Persona("Juan", 25, 0x0000000000000000000000000000000000000000);
+
+//Enums
+enum Estado {
+    Pendiente,
+    Enviado,
+    Recibido,
+    Entregado
+}
+
+Estado estado1 = Estado.Pendiente;
+
+enum Test {
+	Test1,
+	Test2,
+	Test3
+}
+
+//Events
+event Evento(string nombre, uint edad);
+
+//Functions
+function suma(uint a, uint b) public pure returns (uint) {
+    return a + b;
+}
+
+//Modifiers
+address owner = msg.sender;
+address owner2 = msg.sender;
+modifier onlyOwner() {
+    require(msg.sender == owner || msg.sender == owner2);
+    _;
+}
+
+//Events
+event Evento(string nombre, uint edad);
+
+//Functions
+function suma(uint a, uint b) public pure returns (uint) {
+    return a + b;
+}
+}
